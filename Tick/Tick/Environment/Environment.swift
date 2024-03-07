724fae4b8b626db687d34c093557f403c1983c5a
Andre Pham <andrepham33@gmail.com>
2024-03-08 01:43:11 +1100

    Organised project directories, added starter files (environment, string localisation, floating point comparisons)

---
+++ Tick/Tick/Environment/Environment.swift
@@ -0,0 +1,72 @@
+//
+//  Environment.swift
+//  Tick
+//
+//  Created by Andre Pham on 8/3/2024.
+//
+
+import Foundation
+import UIKit
+
+class Environment {
+    
+    public static let inst = Environment()
+    
+    private var safeAreaInsets: UIEdgeInsets? {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return nil
+        }
+        return windowScene.windows.first?.safeAreaInsets
+    }
+    
+    public var deviceType: UIUserInterfaceIdiom {
+        return UIDevice.current.userInterfaceIdiom
+    }
+    
+    public var deviceIsMac: Bool {
+        return ProcessInfo.processInfo.isiOSAppOnMac
+    }
+    
+    public var topSafeAreaHeight: CGFloat {
+        return self.safeAreaInsets?.top ?? 0.0
+    }
+    
+    public var bottomSafeAreaHeight: CGFloat {
+        return self.safeAreaInsets?.bottom ?? 0.0
+    }
+    
+    public var screenHeight: CGFloat {
+        if self.deviceIsMac,
+           let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            return window.frame.size.height
+        } else {
+            return UIScreen.main.bounds.height
+        }
+    }
+    
+    public var screenWidth: CGFloat {
+        if self.deviceIsMac,
+           let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            return window.frame.size.width
+        } else {
+            return UIScreen.main.bounds.width
+        }
+    }
+    
+    public var window: UIWindow? {
+        return (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first
+    }
+    
+    public var deviceIsTiny: Bool {
+        return isLessOrEqual(self.screenHeight, 667)
+    }
+    
+    public var deviceIsLarge: Bool {
+        return self.deviceType != .phone
+    }
+    
+    private init() { }
+    
+}
