950e5fce802baba0995dfdc48a78780928c653e1
Andre Pham <andrepham33@gmail.com>
2024-03-08 06:56:06 +1100

    Replaced start/end Dates with DateInterval

--- Tick/Tick/Model Layer/Task.swift
+++ Tick/Tick/Model Layer/Task.swift
@@ -11,26 +11,23 @@ class Task {
     
     private(set) var title: String
     private(set) var description: String
-    private(set) var startDate: Date
-    private(set) var endDate: Date
+    private(set) var ongoingDuration: DateInterval
     private(set) var markedComplete: Bool
     public var status: TaskStatus {
         if self.markedComplete {
             return .completed
         }
-        let now = Date()
-        let nowIsBeforeStartDate = now < self.startDate
+        let nowIsBeforeStartDate = Date() < self.ongoingDuration.start
         return nowIsBeforeStartDate ? .upcoming : .ongoing
     }
     public var canBeEdited: Bool {
         return self.status == .ongoing
     }
     
-    init(title: String, description: String, startDate: Date, endDate: Date, markedComplete: Bool) {
+    init(title: String, description: String, ongoingDuration: DateInterval, markedComplete: Bool) {
         self.title = title
         self.description = description
-        self.startDate = startDate
-        self.endDate = endDate
+        self.ongoingDuration = ongoingDuration
         self.markedComplete = markedComplete
     }
     
